66782a3fed658c7c045c2292e710cd97dfcfb10d
sidney2k1 <sidsingla02@gmail.com>
2025-01-09 17:12:10 +0530

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+def checkifsame(n1,n2):
+    if n1^n2!=0:
+        print("they are not equal")
+    else:
+        print("They are equal")
+n1=int(input("Enter a number:"))
+n2=int(input("Enter a number"))
+checkifsame(n1,n2)
